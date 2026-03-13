f51b35a83151bf5d770b17a9defbff1b495e11de
Harsh Tanwar <harshtanwar14122003@gmail.com>
2026-03-14 02:57:56 +0530

    commiting 13th March

--- Week_10_9th_Mar_to_14th_Mar_2026/Day_5_13th_Mar_2026/PublishingManagement/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PublishingManagement.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
